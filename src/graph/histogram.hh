d199d4bbb5e20130cc90374b74fac026301812b7
Tiago de Paula Peixoto <tiago@skewed.de>
2007-06-30 00:23:28 +0000

    graph-tool is now GPL v3
    git-svn-id: https://svn.forked.de/graph-tool/trunk@101 d4600afd-f417-0410-95de-beed9576f240

--- src/graph/histogram.hh
+++ src/graph/histogram.hh
@@ -1,10 +1,10 @@
 // graph-tool -- a general graph modification and manipulation thingy
 //
-// Copyright (C) 2006  Tiago de Paula Peixoto <tiago@forked.de>
+// Copyright (C) 2007  Tiago de Paula Peixoto <tiago@forked.de>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
+// as published by the Free Software Foundation; either version 3
 // of the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
