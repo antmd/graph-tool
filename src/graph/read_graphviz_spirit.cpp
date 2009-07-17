87b699272aaeb2dc5029affe680914093ab5bab0
Tiago de Paula Peixoto <tiago@skewed.de>
2009-07-17 18:04:18 +0200

    Sync graphviz_spirit with boost 1.39

--- src/graph/read_graphviz_spirit.cpp
+++ src/graph/read_graphviz_spirit.cpp
@@ -5,32 +5,35 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //
-// read_graphviz_spirit.hpp -
+// read_graphviz_spirit.hpp - 
 //   Initialize a model of the BGL's MutableGraph concept and an associated
 //  collection of property maps using a graph expressed in the GraphViz
-// DOT Language.
+// DOT Language.  
 //
 //   Based on the grammar found at:
 //   http://www.graphviz.org/cvs/doc/info/lang.html
 //
-//   See documentation for this code at:
+//   See documentation for this code at: 
 //     http://www.boost.org/libs/graph/doc/read-graphviz.html
 //
 
 // Authors: Ronald Garcia and Douglas Gregor
 //
 
+#define BOOST_GRAPH_SOURCE
+
 #ifndef BOOST_GRAPH_READ_GRAPHVIZ_ITERATORS
 #  define BOOST_GRAPH_READ_GRAPHVIZ_ITERATORS
 #endif
 #include <boost/graph/graphviz.hpp>
 
-namespace boost { namespace detail { namespace graph {
+namespace boost { namespace detail { namespace graph { 
 
-bool read_graphviz(std::istream& in, mutate_graph& graph)
+BOOST_GRAPH_DECL
+bool read_graphviz(std::istream& in, mutate_graph& graph) 
 {
   using namespace boost;
-  using namespace boost::spirit;
+  using namespace boost::spirit::classic;
 
   typedef std::istream_iterator<char> is_t;
   typedef multi_pass<is_t> iterator_t;
