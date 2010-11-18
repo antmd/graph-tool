ddd2c98f0dfc6785e474f1237b23e73763e4749f
Tiago de Paula Peixoto <tiago@skewed.de>
2010-11-18 14:09:52 +0100

    Small clarification in include order

--- src/graph/graph_bind.cc
+++ src/graph/graph_bind.cc
@@ -15,11 +15,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "graph.hh"
 #define NUMPY_EXPORT
+#include "numpy_bind.hh"
+
+#include "graph.hh"
 #include "graph_python_interface.hh"
 #include "graph_util.hh"
-#include "numpy_bind.hh"
 
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
