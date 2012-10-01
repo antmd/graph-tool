ae2b9c4f9c6dc5b06fec605c2fc848fb6fe20a04
Tiago de Paula Peixoto <tiago@skewed.de>
2012-10-01 11:15:04 +0200

    Fix bug with edge indexes in transitive_closure()
    This fixes ticket #111

--- src/graph/topology/graph_transitive_closure.cc
+++ src/graph/topology/graph_transitive_closure.cc
@@ -39,4 +39,5 @@ void transitive_closure(GraphInterface& gi, GraphInterface& tcgi)
     run_action<graph_tool::detail::always_directed>()
         (gi, bind<void>(get_transitive_closure(), _1,
                         ref(tcgi.GetGraph())))();
+    tcgi.ReIndexEdges();
 }
