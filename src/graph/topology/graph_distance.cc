9a5891076c57bc5641c339943c7fcdca52b5e02a
Tiago de Paula Peixoto <tiago@skewed.de>
2013-01-06 18:32:32 +0100

    Update copyright information

--- src/graph/topology/graph_distance.cc
+++ src/graph/topology/graph_distance.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2006-2012 Tiago de Paula Peixoto <tiago@skewed.de>
+// Copyright (C) 2006-2013 Tiago de Paula Peixoto <tiago@skewed.de>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- src/graph_tool/generation/__init__.py
+++ src/graph_tool/generation/__init__.py
@@ -3,7 +3,7 @@
 #
 # graph_tool -- a general graph manipulation python module
 #
-# Copyright (C) 2007-2012 Tiago de Paula Peixoto <tiago@skewed.de>
+# Copyright (C) 2006-2013 Tiago de Paula Peixoto <tiago@skewed.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.s
 
 """
 ``graph_tool.generation`` - Random graph generation
