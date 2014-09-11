4d8ac23db310f7ac893882e16f45f53094946205
Tiago de Paula Peixoto <tiago@skewed.de>
2014-09-11 16:39:13 +0200

    Fix issue with python 3 in some platforms

--- src/graph_tool/run_action/inline.py
+++ src/graph_tool/run_action/inline.py
@@ -60,10 +60,7 @@ while len(incs) > 0:
     if os.path.isdir(inc):
         incs += glob.glob(inc + "/*")
     else:
-        try:
-            headers_hash = hashlib.md5((headers_hash + open(inc).read()).encode('utf-8')).hexdigest()
-        except UnicodeDecodeError:
-            headers_hash = hashlib.md5(headers_hash + open(inc).read()).hexdigest()
+        headers_hash = hashlib.md5(headers_hash.encode("utf-8") + open(inc, "rb").read()).hexdigest()
 
 # property map types
 props = """
